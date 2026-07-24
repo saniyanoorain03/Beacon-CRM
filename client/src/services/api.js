24032714de7d030e6c3d7a7ebca1efc9bc502406
Saniya D <saniyad31@gmail.com>
2026-07-25 03:27:14 +0530

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://beacon-crm-ej4d.onrender.com/api",
 });
 
 // Attach JWT automatically
